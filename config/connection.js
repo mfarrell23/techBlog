3f05a69a5d6da11b47a98a2efd8181e17f65ee0f
michelle <113373744+mfarrell23@users.noreply.github.com>
2022-11-09 16:37:15 -0700

    fixing connections

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,16 @@ require('dotenv').config();
 
 let sequelize;
 //.env variables
-console.log('this is my db name ' + process.env.DB_NAME)
-if (process.env.blogdb_URL) {
-  sequelize = new Sequelize(process.env.blogdb_URL);
-} else {
-  sequelize = new Sequelize(
-  // console.log(process.env.JAWSDB_URL)
-  // if (process.env.JAWSDB_URL !== undefined) {
-  //   sequelize = new Sequelize(process.env.JAWSDB_URL);
-  // } else {
-  //   sequelize = new Sequelize(
+// console.log('this is my db name ' + process.env.DB_NAME)
+// if (process.env.blogdb_URL) {
+//   sequelize = new Sequelize(process.env.blogdb_URL);
+// } else {
+  // sequelize = new Sequelize(
+  console.log(process.env.JAWSDB_URL)
+  if (process.env.JAWSDB_URL !== undefined) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    sequelize = new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
       process.env.DB_PASSWORD,
