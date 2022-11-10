bf47bbd18c7edf93a1fb107c3c725e861a419c48
michelle <113373744+mfarrell23@users.noreply.github.com>
2022-11-09 17:19:30 -0700

    change config 2

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,9 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-//.env variables
-// console.log('this is my db name ' + process.env.DB_NAME)
-// if (process.env.blogdb_URL) {
-//   sequelize = new Sequelize(process.env.blogdb_URL);
-// } else {
-  // sequelize = new Sequelize(
+
   console.log(process.env.JAWSDB_URL)
-  if (process.env.JAWSDB_URL !== undefined) {
+  if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
   } else {
     sequelize = new Sequelize(
